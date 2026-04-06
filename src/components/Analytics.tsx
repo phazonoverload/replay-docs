66038583378ed90d045967192836c82b308e46fd
Strider Wilson <strider.wilson@replay.io>
2026-04-06 10:36:52 -0400

    Update log rocket app id

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -4,9 +4,13 @@ import { useEffect } from 'react'
 import LogRocket from 'logrocket'
 import mixpanel from 'mixpanel-browser'
 
+const LOGROCKET_APP_ID = 'woocwd/replay-marketing-site-docs'
+
 export default function Analytics() {
   useEffect(() => {
-    LogRocket.init('4sdo4i/replay-docs')
+    if (process.env.NODE_ENV === 'production') {
+      LogRocket.init(LOGROCKET_APP_ID)
+    }
     mixpanel.init('ffaeda9ef8fb976a520ca3a65bba5014', {
       track_pageview: 'url-with-path',
     })
